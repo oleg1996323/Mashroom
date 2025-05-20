93a939c34a048f806b77eff879041b25d1b93996
oster <oleg.ternikov@gmail.com>
2025-05-21 00:28:54 +0300

    Nothing

--- utility/harddrive.h
+++ utility/harddrive.h
@@ -6,40 +6,40 @@
 #include <stdlib.h>
 #include <string>
 
-dev_t get_file_device(const char* path) {
-    struct stat st;
-    if (stat(path, &st) != 0) {
-        return 0;
-    }
-    return st.st_dev;  // Возвращает ID устройства
-}
+// dev_t get_file_device(const char* path) {
+//     struct stat st;
+//     if (stat(path, &st) != 0) {
+//         return 0;
+//     }
+//     return st.st_dev;  // Возвращает ID устройства
+// }
 
-std::string find_mount_device(dev_t dev) {
-    FILE *fp = fopen("/proc/self/mountinfo", "r");
-    if (!fp) {
-        perror("fopen failed");
-        return std::string();
-    }
+// std::string find_mount_device(dev_t dev) {
+//     FILE *fp = fopen("/proc/self/mountinfo", "r");
+//     if (!fp) {
+//         perror("fopen failed");
+//         return std::string();
+//     }
 
-    char line[1024];
-    while (fgets(line, sizeof(line), fp)) {
-        unsigned int maj, min;
-        char device[256];
-        if (sscanf(line, "%*d %*d %u:%u %*s %*s %*s %*s %*s %*s %s", &maj, &min, device) == 3) {
-            if (maj == major(dev) && min == minor(dev)) {
-                fclose(fp);
-                return std::string(device);  // Нашли устройство!
-            }
-        }
-    }
+//     char line[1024];
+//     while (fgets(line, sizeof(line), fp)) {
+//         unsigned int maj, min;
+//         char device[256];
+//         if (sscanf(line, "%*d %*d %u:%u %*s %*s %*s %*s %*s %*s %s", &maj, &min, device) == 3) {
+//             if (maj == major(dev) && min == minor(dev)) {
+//                 fclose(fp);
+//                 return std::string(device);  // Нашли устройство!
+//             }
+//         }
+//     }
 
-    fclose(fp);
-    return std::string();  // Не найдено
-}
+//     fclose(fp);
+//     return std::string();  // Не найдено
+// }
 
-bool is_at_hdd(int fd){
-    bool is_rotational = (ioctl(fd, BLKROTATIONAL, &rot) == 0 && rot);
-}
+// bool is_at_hdd(int fd){
+//     bool is_rotational = (ioctl(fd, BLKROTATIONAL, &rot) == 0 && rot);
+// }
     //TODO for SSD/HDD
     //bool is_rotational = (ioctl(fd, BLKROTATIONAL, &rot) == 0 && rot;
     //__builtin_prefetch(data + offset);
